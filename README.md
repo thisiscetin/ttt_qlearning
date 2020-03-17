1d25c1de5bc86544ae93772f3de5ff68e8ea30d2
Mehmet Cetin <mcetin.cm@gmail.com>
2020-03-17 19:04:48 +0700

    readme update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # TicTacToe game with Q-Learning
 
-Aim of this project to build a model-free reinforcement learning algorithm (QLearning) that can play tic tac toe at super human level.
+Aim of this project to build a model-free reinforcement learning algorithm (QLearning) that can play tic tac toe 
+better than a human does.
+
+When program is run, the agent starts playing against a dummy agent. The agent's moves are backed by QTable, while
+the dummy agent always makes random moves.
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "trainer.h"
 
 int main() {
-    strategy* s = new strategy {0.5, 0.2, 4};
+    strategy* s = new strategy {0.4, 0.3, 20};
     agent* a = new agent(marker::x, s);
     dummy_agent* d = new dummy_agent();
 
@@ -10,6 +10,7 @@ int main() {
 
     int counter = 0;
     int lastWon = 0;
+    int lastLost = 0;
     int lastCheckPoint = 0;
     while (true) {
         counter++;
@@ -18,9 +19,11 @@ int main() {
         if (counter % 100000 == 0) {
             const trainer_stats &s = t->get_stats();
             std::cout << "agent won %: " << float(s.agent_won - lastWon) * 100 / float(counter - lastCheckPoint);
-            std::cout << " table (action) size: " << t->get_table_size() << std::endl;
+            std::cout << ", agent lost %: " << float(s.dummy_agent_won - lastLost) * 100 / float(counter - lastCheckPoint);
+            std::cout << "  | table (action) size: " << t->get_table_size() << std::endl;
 
             lastWon = s.agent_won;
+            lastLost = s.dummy_agent_won;
             lastCheckPoint = counter;
         }
     }
